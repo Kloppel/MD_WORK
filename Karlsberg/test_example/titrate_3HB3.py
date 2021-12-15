6880aab4a706a4b66f056d927152b0c8dd21281d
Kloppel <jesse-jones@live.de>
2021-12-15 14:00:26 +0100

    Added PDB file of 3HB3, exec opt added

--- Karlsberg/test_example/titrate_3HB3.py
+++ Karlsberg/test_example/titrate_3HB3.py
@@ -9,18 +9,18 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     #############
     ### CALCS ###
     #############
-
+    #top = topology
     top = []
-    top.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/top_alw_clean_kb.inp")
-    top.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/patches.rtf")
-    top.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/top_all36_lipid.rtf")
-    top.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/top_all36_cgenff.rtf")
-
+    top.append("/toppar27/top_alw_clean_kb.inp")#here, but what is the difference between top_alw_clean.inp und top_alw_clean_kb.inp
+    top.append("/toppar27/patches.rtf")#here, but unsure if right version
+    top.append("/toppar27/top_all36_lipid.rtf")#here
+    top.append("/toppar27/top_all36_cgenff.rtf")#here
+    #par = parameters
     par = []
-    par.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/par_all22_prot_plus_heme_and_Cu_kb.inp")
-    par.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/patches.prm")
-    par.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/par_all36_lipid.prm")
-    par.append("/scratch/scratch/jdragelj/CHARMM_TOPPAR/par_all36_cgenff.prm")
+    par.append("/toppar27/par_all22_prot_plus_heme_and_Cu_kb.inp")#here but what is the diference between regular and _kb file?
+    par.append("/toppar27/patches.prm")#here
+    par.append("/toppar27/par_all36_lipid.prm")#here
+    par.append("/toppar27/par_all36_cgenff.prm")#here
 
 
     kbp2_settings = kbp2.pka_calculation.PkaCalcSettings()
@@ -43,12 +43,7 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     # kbp2_settings.cavity_par = [0.8, 0.2, 0.0]
 
     # kbp2_settings.titr_residue_charges_0 = True
-    # titratable_yaml = '/scratch/scratch/tmeyer/md_pka/runs/titratables/titratable_refsmall_shifted_ter2_np.yaml'
-
-    # titratable_yaml = '/user/jdragelj/python/karlsberg/kbp2/additional_files/titratable.yaml'
-    # titratable_yaml = '/scratch/scratch/jdragelj/KB_files/titratable_cco.yaml'
-    titratable_yaml = '/scratch/scratch/jdragelj/KB_files/titratable_cco_noglupasp.yaml'
-
+    titratable_yaml = 'toppar27/titratable_cco_noglupasp.yaml'
     kbp2_settings.set_yaml(titratable_yaml)
     titratable_definitions = kbp2.kbp_tools.parse_titratable_yaml(titratable_yaml)
     ##############################################################################
@@ -157,7 +152,7 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     kbp2_settings.preopt = { 'carb_oxi_relax' : False, \
                             'init_die_4' :  False \
                             }
-    kbp2_settings.tapbs_bin = 'LD_LIBRARY_PATH=/user/jdragelj/bin /scratch/scratch/tmeyer/kbplus2/tapbs_1.3_cav_enere'
+    kbp2_set.tapbs_bin = 'LD_LIBRARY_PATH=/home/pbuser/Desktop/PhD_WORK/TARs/kb2plus_package/lib_fort_gnu /home/pbuser/Desktop/PhD_WORK/TARs/kb2plus_package/bin/tapbs_1.3_cav_enere'
     kbp2_settings.remove_folders = 'all'
     kbp2_settings.set_structure(pdb_structure)
     kbp2_settings.set_workdir(workfolder)
@@ -169,22 +164,27 @@ if __name__ == '__main__':
 
     print('for project with "Maria Andrea and Inez Weidinger')
 
-    # workfolder = '/scratch/scratch/jdragelj/projects/cco_model_pls_prd/titrate_2GSM/OO_cavities/'
-    # pdb_structure = '/scratch/scratch/jdragelj/projects/cco_model_pls_prd/titrate_2GSM/cco_2gsm.pdb'
-    # state = '21Cua_3hea_3hea3_2Cub'
-    # print 'state is OO'
-    # titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
+    #workfolder = '/test/titrate_3HB3/'
+    #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
+    #state = '21Cua_3hea_3hea3_2Cub'
+    #print('state is OO')
+    #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
 
-    # workfolder = '/scratch/scratch/jdragelj/projects/cco_model_pls_prd/titrate_2GSM/RO/'
-    # pdb_structure = '/scratch/scratch/jdragelj/projects/cco_model_pls_prd/titrate_2GSM/cco_2gsm.pdb'
-    # state = '11Cua_2hea_3hea3_2Cub'
-    # print 'state is RO'
-    # titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
+    #workfolder = '/test/titrate_3HB3/'
+    #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
+    #state = '11Cua_2hea_3hea3_2Cub'
+    #print('state is RO')
+    #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
 
-    # workfolder = '/scratch/scratch/jdragelj/projects/cco_pls_prd/titrate_2GSM/RR/'
-    # pdb_structure = '/scratch/scratch/jdragelj/projects/cco_pls_prd/titrate_2GSM/cco_2gsm.pdb'
-    # state = '11Cua_2hea_2hea3_1Cub'
-    # print 'state is RR'
-    # titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
+    #workfolder = '/test/titrate_3HB3/'
+    #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
+    #state = '11Cua_2hea_2hea3_1Cub'
+    #print('state is RR')
+    #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
+
+    workfolder = '/test/titrate_3HB3/'
+    pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
+    state = '21Cua_3hea_3hea3_2Cub'
+    titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
 
 
