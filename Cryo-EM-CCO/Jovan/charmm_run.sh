11db1ef391feb5d8b973604e044c9cd01b209000
Kloppel <jesse-jones@live.de>
2021-12-17 14:47:55 +0100

    Further Modifications to Karlsberg-3HB3 script, rebuilt 3HB3 for sim

--- Cryo-EM-CCO/Jovan/charmm_run.sh
+++ Cryo-EM-CCO/Jovan/charmm_run.sh
@@ -3,4 +3,4 @@
 cd  /scratch/scratch/jdragelj/projects/cco_alexiev/MD_299/four_orient_new/mds/90/hsd73_hsd526_flu2/change/ 
 charmm_c39a2q < read_cco.inp > read_cco.out 
 
-        
+
--- Karlsberg/test_example/titrate_3HB3.py
+++ Karlsberg/test_example/titrate_3HB3.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 
 import kbp2
-from workspace_jd import cco_config
+
+#from workspace_jd import cco_config
 
 
 def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
@@ -11,13 +12,13 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     #############
     #top = topology
     top = []
-    top.append("/toppar27/top_alw_clean_kb.inp")#here, but what is the difference between top_alw_clean.inp und top_alw_clean_kb.inp
-    top.append("/toppar27/patches.rtf")#here, but unsure if right version
+    top.append("/toppar27/top_alw_clean.inp")#here, but what is the difference between top_alw_clean.inp und top_alw_clean_kb.inp
+    top.append("/toppar27/patches.rtf")#here
     top.append("/toppar27/top_all36_lipid.rtf")#here
     top.append("/toppar27/top_all36_cgenff.rtf")#here
     #par = parameters
     par = []
-    par.append("/toppar27/par_all22_prot_plus_heme_and_Cu_kb.inp")#here but what is the diference between regular and _kb file?
+    par.append("/toppar27/par_all22_prot_plus_heme_and_Cu.inp")#here but what is the diference between regular and _kb file?
     par.append("/toppar27/patches.prm")#here
     par.append("/toppar27/par_all36_lipid.prm")#here
     par.append("/toppar27/par_all36_cgenff.prm")#here
@@ -43,8 +44,8 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     # kbp2_settings.cavity_par = [0.8, 0.2, 0.0]
 
     # kbp2_settings.titr_residue_charges_0 = True
-    titratable_yaml = 'toppar27/titratable_cco_noglupasp.yaml'
-    kbp2_settings.set_yaml(titratable_yaml)
+    #titratable_yaml = 'toppar27/titratable_cco_noglupasp.yaml'
+    #kbp2_settings.set_yaml(titratable_yaml)
     titratable_definitions = kbp2.kbp_tools.parse_titratable_yaml(titratable_yaml)
     ##############################################################################
 
@@ -63,8 +64,7 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
         charge_patches.append({'CA21': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
                                         'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
         charge_patches.append({'A3H3': ['HEM-2_EHEM', 'HSD-419_ACHA', 'OHMI-1_FEOH']})
-        charge_patches.append(
-            {'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
+        charge_patches.append({'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
         # charge_patches.append({'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'TIP3-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
 
         bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
@@ -81,8 +81,7 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
         charge_patches.append({'CA11': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
                                         'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
         charge_patches.append({'A3H3': ['HEM-2_EHEM', 'HSD-419_ACHA', 'OHMI-1_FEOH']})
-        charge_patches.append(
-            {'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
+        charge_patches.append({'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
 
         bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
         bond_patches.append({'PHEM': ['HSD-102_ACHA', 'HEM-2_GHEM']})
@@ -97,8 +96,7 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
         charge_patches.append({'CA11': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
                                         'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
         charge_patches.append({'A3H2': ['HEM-2_EHEM', 'HSD-419_ACHA', 'OHMI-1_FEOH']})
-        charge_patches.append(
-            {'CBT4': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
+        charge_patches.append({'CBT4': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
 
         bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
         bond_patches.append({'PHEM': ['HSD-102_ACHA', 'HEM-2_GHEM']})
@@ -114,8 +112,7 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
                                         'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
 
         charge_patches.append({'OA3O': ['HEM-2_EHEM', 'HSD-419_ACHA', 'PER-1_PERI']})
-        charge_patches.append(
-            {'OCBO': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'PER-1_PERI', 'TYR-288_ACHA', 'HSE-284_ACHA']})
+        charge_patches.append({'OCBO': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'PER-1_PERI', 'TYR-288_ACHA', 'HSE-284_ACHA']})
 
 
     if state == 'Pr':
@@ -149,10 +146,10 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     kbp2_settings.set_excluded_residues(['HSD-102_ACHA', 'HSD-421_ACHA', 'HSD-419_ACHA', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA', 'CYS-256_BCHA', \
                                          'HSE-260_BCHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'TYR-288_ACHA', 'HSE-284_ACHA'])
 
-    kbp2_settings.preopt = { 'carb_oxi_relax' : False, \
-                            'init_die_4' :  False \
-                            }
-    kbp2_set.tapbs_bin = 'LD_LIBRARY_PATH=/home/pbuser/Desktop/PhD_WORK/TARs/kb2plus_package/lib_fort_gnu /home/pbuser/Desktop/PhD_WORK/TARs/kb2plus_package/bin/tapbs_1.3_cav_enere'
+    kbp2_settings.preopt = { 'carb_oxi_relax' : False,
+                            'init_die_4' :  True
+                            }#beide false für Titration aus MD, Koordinaten der schweren Atome nicht ändern für Koordinaten aus MD
+    kbp2_settings.tapbs_bin = 'LD_LIBRARY_PATH=/home/pbuser/Desktop/PhD_WORK/TARs/kb2plus_package/lib_fort_gnu /home/pbuser/Desktop/PhD_WORK/TARs/kb2plus_package/bin/tapbs_1.3_cav_enere'
     kbp2_settings.remove_folders = 'all'
     kbp2_settings.set_structure(pdb_structure)
     kbp2_settings.set_workdir(workfolder)
@@ -161,8 +158,12 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
 
 
 if __name__ == '__main__':
+    workfolder = '/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/test/titrate_3HB3/'
+    pdb_structure = '/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/test/titrate_3HB3/3hb3.pdb'
+    state = '21Cua_3hea_3hea3_2Cub'
+    titrate_structure(pdb_structure=pdb_structure, workfolder=workfolder, state=state, residues_tt=None)
 
-    print('for project with "Maria Andrea and Inez Weidinger')
+    #print('for project with "Maria Andrea and Inez Weidinger')
 
     #workfolder = '/test/titrate_3HB3/'
     #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
@@ -181,10 +182,3 @@ if __name__ == '__main__':
     #state = '11Cua_2hea_2hea3_1Cub'
     #print('state is RR')
     #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
-
-    workfolder = '/test/titrate_3HB3/'
-    pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
-    state = '21Cua_3hea_3hea3_2Cub'
-    titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
-
-
