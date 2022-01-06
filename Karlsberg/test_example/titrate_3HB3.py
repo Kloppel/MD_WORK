fd81476843269525d590e18ec71a0ae046f5ec3d
Kloppel <jesse-jones@live.de>
2022-01-06 16:23:41 +0100

    Reformatted titrate_3hb3.py but error persists

--- Karlsberg/test_example/titrate_3HB3.py
+++ Karlsberg/test_example/titrate_3HB3.py
@@ -1,29 +1,25 @@
-# coding=utf-8
-
 import kbp2
-
-#from workspace_jd import cco_config
-
 
 def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
 
     #############
     ### CALCS ###
     #############
+
     #top = topology
     top = []
-    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/top_alw_clean_kb.inp")#here, but what is the difference between top_alw_clean.inp und top_alw_clean_kb.inp
-    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/patches.rtf")#here
-    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/top_all36_lipid.rtf")#here
-    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/top_all36_cgenff.rtf")#here
+    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/top_alw_clean_kb.inp")
+    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/patches.rtf")
+    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/top_all36_lipid.rtf")
+    top.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/top_all36_cgenff.rtf")
     #par = parameters
     par = []
-    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/par_all22_prot_plus_heme_and_Cu_kb.inp")#here but what is the diference between regular and _kb file?
-    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/patches.prm")#here
-    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/par_all36_lipid.prm")#here
-    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/par_all36_cgenff.prm")#here
+    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/par_all22_prot_plus_heme_and_Cu_kb.inp")
+    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/patches.prm")
+    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/par_all36_lipid.prm")
+    par.append("/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/par_all36_cgenff.prm")
 
-
+    #Add Settings
     kbp2_settings = kbp2.pka_calculation.PkaCalcSettings()
     kbp2_settings.top = top
     kbp2_settings.par = par
@@ -31,106 +27,25 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     kbp2_settings.quiet_mode = True
 
     decisions = []
-    # decisions.append(('rename__HOH_TIP3', 'keep'))
+    #decisions.append(('rename__HOH_TIP3', 'keep'))
     decisions.append(('rename__HOH_TIP3', 'remove'))
     decisions.append(('disu_bridges', 'closed'))
     decisions.append(('cap_termini', 'dont_cap'))
-
     kbp2_settings.modelling_decisions = decisions
-
     protocol = []
     protocol.append((  7, 'h_min'))
     kbp2_settings.protocol = protocol
     # kbp2_settings.cavity_par = [0.8, 0.2, 0.0]
-
     # kbp2_settings.titr_residue_charges_0 = True
     titratable_yaml = '/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/toppar27/titratable_cco_noglupasp.yaml'
     kbp2_settings.set_yaml(titratable_yaml)
     titratable_definitions = kbp2.kbp_tools.parse_titratable_yaml(titratable_yaml)
-    ##############################################################################
-
-                    #########
-                    ### 1 ###        # 1. reduces of oxidized enzime
-                    #########
-
 
     kbp2_settings.patches = []
     kbp2_settings.patches_no_autogen = []
     charge_patches = []
     bond_patches = []
 
-    #from here on 2gsm patches
-#    if state == '21Cua_3hea_3hea3_2Cub': #
-#        charge_patches.append({'AHE3': ['HEM-2_GHEM', 'HSD-102_ACHA', 'HSD-421_ACHA']})
-#        charge_patches.append({'CA21': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-#                                        'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#        charge_patches.append({'A3H3': ['HEM-2_EHEM', 'HSD-419_ACHA', 'OHMI-1_FEOH']})
-#        charge_patches.append({'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
-#        # charge_patches.append({'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'TIP3-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
-#
-#        bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
-#        bond_patches.append({'PHEM': ['HSD-102_ACHA', 'HEM-2_GHEM']})
-#        bond_patches.append({'PHE2': ['HSD-421_ACHA', 'HEM-2_GHEM']})
-#        bond_patches.append({'EISO': ['OHMI-1_FEOH', 'HEM-2_EHEM']})
-#        bond_patches.append({'CUB2': ['CU1-1_META', 'HSE-284_ACHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC']})
-#        # bond_patches.append({'CUB2': ['CU1-1_META', 'HSE-284_ACHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'TIP3-1_HOHC']})
-#        bond_patches.append({'CUAP': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-#                                      'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#
-#    if state == '11Cua_2hea_3hea3_2Cub':
-#        charge_patches.append({'AHE2': ['HEM-2_GHEM', 'HSD-102_ACHA', 'HSD-421_ACHA']})
-#        charge_patches.append({'CA11': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-#                                        'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#        charge_patches.append({'A3H3': ['HEM-2_EHEM', 'HSD-419_ACHA', 'OHMI-1_FEOH']})
-#        charge_patches.append({'CBF2': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
-#
-#        bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
-#        bond_patches.append({'PHEM': ['HSD-102_ACHA', 'HEM-2_GHEM']})
-#        bond_patches.append({'PHE2': ['HSD-421_ACHA', 'HEM-2_GHEM']})
-#        bond_patches.append({'EISO': ['OHMI-1_FEOH', 'HEM-2_EHEM']})
-#        bond_patches.append({'CUB2': ['CU1-1_META', 'HSE-284_ACHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC']})
-#        bond_patches.append({'CUAP': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-#                                      'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#
-#    if state == '11Cua_2hea_2hea3_1Cub':
-#        charge_patches.append({'AHE2': ['HEM-2_GHEM', 'HSD-102_ACHA', 'HSD-421_ACHA']})
-#        charge_patches.append({'CA11': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-#                                        'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#        charge_patches.append({'A3H2': ['HEM-2_EHEM', 'HSD-419_ACHA', 'OHMI-1_FEOH']})
-#        charge_patches.append({'CBT4': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
-#
-#        bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
-#        bond_patches.append({'PHEM': ['HSD-102_ACHA', 'HEM-2_GHEM']})
-#        bond_patches.append({'PHE2': ['HSD-421_ACHA', 'HEM-2_GHEM']})
-#        bond_patches.append({'EISO': ['OHMI-1_FEOH', 'HEM-2_EHEM']})
-#        bond_patches.append({'CUB2': ['CU1-1_META', 'HSE-284_ACHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'HOH-1_HOHC']})
-#        bond_patches.append({'CUAP': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-#                                      'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#
-#                                       
-    #from here on 3hb3 patches
-    #if state == 'O2_bound':
-    #    charge_patches.append({'AHE3': ['HEM-2_GHEM', 'HSD-102_ACHA', 'HSD-421_ACHA']})
-    #    charge_patches.append({'CA21': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-    #                                    'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-    #    charge_patches.append({'OA3O': ['HEM-2_EHEM', 'HSD-419_ACHA', 'PER-1_PERI']})
-    #    charge_patches.append({'OCBO': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'PER-1_PERI', 'TYR-288_ACHA', 'HSE-284_ACHA']})
-#
-    #if state == 'Pr':
-    #    charge_patches.append({'AHE3': ['HEM-2_GHEM', 'HSD-102_ACHA', 'HSD-421_ACHA']})
-    #    charge_patches.append({'CA21': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-    #                                    'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-    #    charge_patches.append({'A3H4': ['HEM-2_EHEM', 'HSD-419_ACHA', 'HAO2-1_FEOH']})
-    #    charge_patches.append({'CBPN': ['CU1-1_META', 'HSD-333_ACHA', 'HSD-334_ACHA', 'OMI2-1_HOHC', 'TYR-288_ACHA', 'HSE-284_ACHA']})
-#
-    #    bond_patches.append({'PHEM': ['HSD-419_ACHA', 'HEM-2_EHEM']})
-    #    bond_patches.append({'PHEM': ['HSD-102_ACHA', 'HEM-2_GHEM']})
-    #    bond_patches.append({'PHE2': ['HSD-421_ACHA', 'HEM-2_GHEM']})
-    #    bond_patches.append({'EISO': ['HAO2-1_FEOH', 'HEM-2_EHEM']})
-    #    bond_patches.append({'CUB2': ['CU1-1_META', 'HSE-284_ACHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'OMI2-1_HOHC']})
-    #    bond_patches.append({'CUAP': ['CU1-2_META', 'CU1-3_META', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA',
-    #                                  'CYS-256_BCHA', 'HSE-260_BCHA', 'MET-263_BCHA']})
-#
     if state == 'O':
         ### CHARGE PATCHES
         charge_patches.append({'AHE3' : ['HEM-2_GHEM', 'HSD-94_ACHA', 'HSD-413_ACHA']})
@@ -194,15 +109,12 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
         # new_patch = cco_config.copy_patch(patch)
         kbp2_settings.patches.append(patch)
         # kbp2_settings.patches_no_autogen.append(patch)
-
     #BOND PACTHES -> HEME AND COPPER
     for patch in bond_patches:
         # new_patch = cco_config.copy_patch(patch)
         kbp2_settings.patches_no_autogen.append(patch)
-
     kbp2_settings.set_excluded_residues(['HSD-102_ACHA', 'HSD-421_ACHA', 'HSD-419_ACHA', 'HSE-217_BCHA', 'CYS-252_BCHA', 'GLU-254_BCHA', 'CYS-256_BCHA', \
                                          'HSE-260_BCHA', 'HSD-333_ACHA', 'HSD-334_ACHA', 'TYR-288_ACHA', 'HSE-284_ACHA'])
-
     kbp2_settings.preopt = { 'carb_oxi_relax' : False,
                             'init_die_4' :  True
                             }#beide false für Titration aus MD, Koordinaten der schweren Atome nicht ändern für Koordinaten aus MD
@@ -213,30 +125,9 @@ def titrate_structure(pdb_structure, workfolder, state, residues_tt=None):
     kbp2_settings.apbs_res = 0.3
     kbp2.pka_calculation.calc_pkas(kbp2_settings, titratable_definitions)
 
-
 if __name__ == '__main__':
     workfolder = '/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/test/titrate_3HB3/'
     pdb_structure = '/home/pbuser/Desktop/PhD_WORK/MD_WORK/Karlsberg/test_example/test/titrate_3HB3/3hb3.pdb'
     #state = '21Cua_3hea_3hea3_2Cub'
     state= 'O'
     titrate_structure(pdb_structure=pdb_structure, workfolder=workfolder, state=state, residues_tt=None)
-
-    #print('for project with "Maria Andrea and Inez Weidinger')
-
-    #workfolder = '/test/titrate_3HB3/'
-    #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
-    #state = '21Cua_3hea_3hea3_2Cub'
-    #print('state is OO')
-    #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
-
-    #workfolder = '/test/titrate_3HB3/'
-    #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
-    #state = '11Cua_2hea_3hea3_2Cub'
-    #print('state is RO')
-    #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
-
-    #workfolder = '/test/titrate_3HB3/'
-    #pdb_structure = '/test/titrate_3HB3/3hb3.pdb'
-    #state = '11Cua_2hea_2hea3_1Cub'
-    #print('state is RR')
-    #titrate_structure(pdb_structure, workfolder, state, residues_tt=None)
